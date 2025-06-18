f016173e6351eda90e19365f217a249af7e559c5
Richard Murillo <rjmurillo@gmail.com>
2025-06-18 10:49:28 -0700

    Test WIP

--- tests/System.Runtime.Caching.Analyzers.Tests/SystemRuntimeCachingAnalyzerTests.cs
+++ tests/System.Runtime.Caching.Analyzers.Tests/SystemRuntimeCachingAnalyzerTests.cs
@@ -1,5 +1,8 @@
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Testing;
+using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Testing.Verifiers;
 using Xunit;
 
 namespace System.Runtime.Caching.Analyzers.Tests
@@ -9,12 +12,50 @@ namespace System.Runtime.Caching.Analyzers.Tests
     /// </summary>
     public class SystemRuntimeCachingAnalyzerTests
     {
+        private static DiagnosticResult ExpectedDiagnostic(int line, int column) =>
+            new DiagnosticResult(SystemRuntimeCachingAnalyzer.DiagnosticId, DiagnosticSeverity.Warning)
+                .WithSpan(line, column, line, column + 11); // "MemoryCache" length
+
         [Fact]
         public async Task FlagsMemoryCacheUsageInNetCoreProject()
         {
-            // TODO: Implement test using CSharpAnalyzerTest<TAnalyzer, XUnitVerifier>
-            // This test should fail until analyzer logic is implemented.
-            await Task.FromException(new System.NotImplementedException());
+            var testCode = @"using System.Runtime.Caching;
+
+class C
+{
+    void M()
+    {
+        var cache = new MemoryCache(\"test\");
+    }
+}";
+            var test = new CSharpAnalyzerTest<SystemRuntimeCachingAnalyzer, XUnitVerifier>
+            {
+                TestCode = testCode,
+                ReferenceAssemblies = ReferenceAssemblies.Net.Net50
+            };
+            test.ExpectedDiagnostics.Add(ExpectedDiagnostic(7, 22));
+            await test.RunAsync();
+        }
+
+        [Fact]
+        public async Task DoesNotFlagMemoryCacheUsageInNetFramework()
+        {
+            var testCode = @"using System.Runtime.Caching;
+
+class C
+{
+    void M()
+    {
+        var cache = new MemoryCache(\"test\");
+    }
+}";
+            var test = new CSharpAnalyzerTest<SystemRuntimeCachingAnalyzer, XUnitVerifier>
+            {
+                TestCode = testCode,
+                ReferenceAssemblies = ReferenceAssemblies.NetFramework.Net48
+            };
+            // No diagnostics expected
+            await test.RunAsync();
         }
     }
 } 
